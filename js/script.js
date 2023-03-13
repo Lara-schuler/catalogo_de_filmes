93957b1490a2f6e0d0f586cedc0784474152881f
Lara Schuler <lara-schuler@educar.rs.gov.br>
2023-03-13 20:21:25 -0300

    adicionando novas alterações na nova branch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let ator = new Ator(1, "JOHN WAYNE");
+/*let ator = new Ator(1, "JOHN WAYNE");
 console.log(ator);
 
 let diretor = new Diretor(1,"Alfred Hitchock");
@@ -91,6 +91,54 @@ const carregaLista = (json) =>{
         lista.appendChild(item);
         
     });
+}*/
+
+let inputBuscarFilme = document.querySelector('#input-buscar-filme');
+let btnBuscarFilme = document.querySelector('#btn-buscar-filme');
+const apikey =  '8c76fa7d';
+
+btnBuscarFilme.onclick = () => {
+    if(inputBuscarFilme.value.length > 0) {
+       fetch('https://www.omdbapi.com/?&apikey=8c76fa7d&s='+inputBuscarFilme.value, {mode:"cors"})
+       .then((resp) => resp.json()) 
+       .then((resp) => {
+            console.log(resp);
+       })
+    }
+    return false;
 }
 
+getCard = async () => {
+    let card = document.createElement('div');
+    card.setAttribute('class','card');
+    let imgCartaz = document.createElement('img');
+    imgCartaz.setAttribute('class','card-img-topz');
+    imgCartaz.setAttribute('src', this.cartaz);
+    let cardBody = document.createElement('div');
+    cardBody.setAttribute('class','card-body');
+    hCardTitle = document.createElement('h5');
+    hCardTitle.setAttribute('class','card-title');
+    let divDetalhes = document.createElement('div');
+    divDetalhes.setAttribute('style','display:flex; justify-content:space-around;');
+    let divGenero = document.createElement('div');
+    divGenero.setAttribute('style', 'flex-grow:1;');
+    let divAnoDeProducao = document.createElement('div');
+    divAnoDeProducao.setAttribute('style', 'flex-grow:1;');
+    let divClassificacao = document.createElement('div');
+    divClassificacao.setAttribute('style', 'flex-grow:1;');
+    hCardTitle.appendChild(document.createTextNode(this.titulo));
+    divGenero.appendChild(document.createTextNode(this.genero));
+    divAnoDeProducao.appendChild(document.createTextNode(this.ano));
+    divClassificacao.appendChild(document.createTextNode(this.classificacao));
+    divDetalhes.appendChild(divGenero);
+    divDetalhes.appendChild(divAnoDeProducao);
+    divDetalhes.appendChild(divClassificacao);
+    card.appendChild(imgCartaz);
+    card.appendChild(cardBody);
+    cardBody.appendChild(hCardTitle);
+    cardBody.appendChild(divDetalhes);
+    return card;
+
+
+}
 
